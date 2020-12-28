5b4883db8f379248b87c038a47cfffa3a7953130
Morteza gholam <gholam.morteza.9821164203@gmail.com>
2020-12-28 12:58:06 +0330

    change

--- upload.sh
+++
@@ -1,5 +0,0 @@
-find  . \( -name '*~' -or -name '*.pyc' -or -name '__pycache__' -or -name '.~lock.*.csv#' -or -name 'a.out' \) -delete -print
-
-git add .
-git commit -am "date: `date` "
-git push origin main
