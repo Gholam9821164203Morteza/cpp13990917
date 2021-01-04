638e9b4fb1bbddef7ce6a14267a30036fe660618
Morteza gholam <gholam.morteza.9821164203@gmail.com>
2021-01-04 09:45:18 +0330

    text file

--- cpp.cpp
+++ cpp.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include<fstream>
+#include<iostream>
 using namespace std;
-int main()
-{
-	cout << "Hello world." ;
+
+int main(){
+    int i;
+    ofstream f1("2.out.txt");
+    for(i = 0; i < 10; i++){
+    	if(i%2==0)
+    	 f1 << i << endl; 
+	}
+    f1 << "Morteza Gholam" << endl;
+    f1.close();
+    return 0;
 }
